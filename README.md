3045be80e75f931c4b8fa594d664f477975ba928
Vladimír Jarabica <vladojarabica@gmail.com>
2020-04-22 10:00:27 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# poker-transcendence
+# Poker Transcendence
+
+Basically a key logger but with good will.
+
+Generates a random number for each click in range from 1 to 100.
---
+++ index.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, ipcMain } = require("electron");
+const ioHook = require("iohook");
+
+app.allowRendererProcessReuse = true;
+
+app.on("ready", () => {
+  const win = new BrowserWindow({
+    width: 100,
+    height: 100,
+    webPreferences: { nodeIntegration: true },
+    alwaysOnTop: true,
+    frame: null,
+  });
+
+  win.loadURL(`file://${__dirname}/index.html`);
+
+  win.removeMenu();
+
+  win.webContents.on("did-finish-load", () => {
+    win.webContents.send("ping", "");
+    console.log("did finish");
+  });
+
+  ioHook.on("mousedown", (event) => {
+    win.webContents.send("ping", "");
+  });
+
+  ioHook.start();
+});
+
+process.on("exit", () => {
+  ioHook.unload();
+});
